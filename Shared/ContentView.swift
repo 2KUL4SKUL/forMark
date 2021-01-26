8d2fc50d6358bbda0c913b2cb53fa2e677d23e65
a.m.student <minorbrosadam@gmail.com>
2021-01-26 08:38:52 -0600

    basic grid

---
+++ Shared/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by A.M. Student on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView{
+        Home()
+            .navigationTitle("Tic Tac Toe")
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
+                
+                ForEach(0..<9, id: \.self) {index in
+                    
+                    Color.blue
+                }
+                
+            }
+        }
+        
+        .padding(15)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
